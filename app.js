9f9eadb7220cce5a3d8dc55aef7f99311c59d8f8
Zack Lyon <zack.j.lyon@gmail.com>
2021-09-28 14:28:18 -0700

    Add endpoints

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const { thingQuotes } = require('./data.js');
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.use(cors());
+
+app.get('/thing-quotes', (req, res) => {
+    res.json(thingQuotes);
+})
+
+app.get('/thing-quotes/:id', (req, res) => {
+    const match = thingQuotes.find(quote => quote.id === Number(req.params.id));
+
+    res.json(match)
 })
 
 module.exports = {
     app
 }
+
+//git push heroku dev:main
